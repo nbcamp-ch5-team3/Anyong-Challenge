7ecd8dfc4e31f4beefdc88338d1dca4acf3c18de
ChoiAnYong <dksdyd78@naver.com>
2025-05-13 15:48:37 +0900

    add: 프로젝트 세팅

---
+++ iTunesApp/iTunesApp/Presentation/View/MainScene/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iTunesApp
+//
+//  Created by 최안용 on 5/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
